02db8af1be2c5211661f3d78c8ba80573e9ee140
raven <ravenmetals@gmail.com>
2016-09-15 22:47:46 -0500

    starting tile placement

--- PressYourLuck/PressYourLuck/PressYourLuckForm.Designer.cs
+++ PressYourLuck/PressYourLuck/PressYourLuckForm.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GameStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // GameStart
+            // 
+            this.GameStart.Location = new System.Drawing.Point(728, 495);
+            this.GameStart.Name = "GameStart";
+            this.GameStart.Size = new System.Drawing.Size(75, 23);
+            this.GameStart.TabIndex = 0;
+            this.GameStart.Text = "StartGame";
+            this.GameStart.UseVisualStyleBackColor = true;
+            this.GameStart.Click += new System.EventHandler(this.GameStart_Click);
             // 
             // PressYourLuckForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(815, 530);
+            this.Controls.Add(this.GameStart);
             this.Name = "PressYourLuckForm";
             this.Text = "Press You Luck";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button GameStart;
     }
 }
 
--- PressYourLuck/PressYourLuck/PressYourLuckForm.cs
+++ PressYourLuck/PressYourLuck/PressYourLuckForm.cs
@@ -12,9 +12,42 @@ namespace PressYourLuck
 {
     public partial class PressYourLuckForm : Form
     {
+        private PictureBox[] tileArray;
+        private Point[] tileLocations;
+
         public PressYourLuckForm()
         {
             InitializeComponent();
+            tileArray = new PictureBox[8];
+        }
+
+        public ICollection<PictureBox> TileArray => tileArray;
+
+        private void GameStart_Click(object sender, EventArgs e)
+        {
+            // set on offset to skip center square only needed here for xy location
+            var offset = 0;
+            //loop 3 rows 3 colomns
+            for (int i = 0; i < 3 ; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //calculate the actual array index 
+                    var index = (i*3) + j;
+                    if (index == 4)
+                    {
+                        offset = 1;
+                        continue;
+                    }
+                    // create the new picturebox at index location ie tileArray[2]
+                    tileArray[index - offset] = new PictureBox { Location = new Point(j * 50, i * 50), Enabled = true,
+                        Size = new Size(45, 45), SizeMode = PictureBoxSizeMode.StretchImage};
+                    //add picturebox to form
+                    this.Controls.Add(tileArray[index -offset]);
+                    //set an image so it can be seen on board
+                    this.tileArray[index - offset].ImageLocation = @"images\mayorWhammy.png";
+                }
+            }
         }
     }
 }
